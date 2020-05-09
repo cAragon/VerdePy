d373fe3ef212e1f4306c5c05ec53a2aa1b56ea4b
cAragon <cAragon@users.noreply.github.com>
2020-05-09 11:32:46 -0300

    Add files via upload

---
+++ verde_checkFiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#VERIFICACAO DE ARQUIVOS DE SISTEMA
+
+#verifica
+
+#caso existam
+
+#caso não existam
+
+#CRIE DIRETÓRIO PRINCIPAL;
+
+cd ~/Documents; mkdir VerdeAoCubo; cd VerdeAoCubo
+
+#CLONE REPOSITÓRIO DE ARQUIVOS DE SISTEMA
+
+git clone https://github.com/cAragon/VerdePy; ls
